519c07daae53d5e7559d8dbad8961d393d245c82
Jean <oka.jeanjaures@gmail.com>
2026-03-03 11:52:34 +0100

    translation config for datepicker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
@@ -9,9 +14,17 @@ import Aura from '@primeuix/themes/aura';
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {BasicAuthInterceptor} from './core/interceptors/basic-auth-interceptor';
 import {ResponseWrapperInterceptor} from './core/interceptors/response-wrapper-interceptor';
+import localeFr from '@angular/common/locales/fr';
+import {registerLocaleData} from '@angular/common';
 
-export const appConfig: ApplicationConfig = {
+
+
+registerLocaleData(localeFr);
+
+
+  export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'fr' },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: BasicAuthInterceptor,
@@ -27,12 +40,25 @@ export const appConfig: ApplicationConfig = {
     ToastModule,
     provideAnimationsAsync(),
     providePrimeNG({
+        //  locale: fr,
       theme: {
         preset: Aura,
         options:{
           prefix: 'p',
           darkModeSelector: 'none'
         }
+      },
+      // Translation config
+      translation: {
+        dayNames: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+        dayNamesShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+        dayNamesMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
+        monthNames: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+        monthNamesShort: ["Jan", "Fév", "Mar", "Avr", "Mai", "Jun", "Jul", "Aoû", "Sep", "Oct", "Nov", "Déc"],
+        today: 'Aujourd\'hui',
+        clear: 'Effacer',
+        dateFormat: 'dd/mm/yy',
+        firstDayOfWeek: 1
       }
     }),
     provideBrowserGlobalErrorListeners(),
