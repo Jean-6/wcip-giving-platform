db1aa0bbb24a1b7de43c9236f569d3d87e7151ed
a1ag02 <jean-jaures_oka@smabtp.fr>
2026-06-18 11:14:20 +0200

    landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import {Donation} from './features/donation/donation';
 import {TransactionReceipt} from './features/transaction-receipt/transaction-receipt';
+import {Home} from './features/home/home';
 
 
 export const routes: Routes = [
 
+
+  {
+    path: '',
+    component: Home
+  },
   {
     path: 'donation',
     component: Donation
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Toast} from 'primeng/toast';
+import {Header} from './components/header/header';
+import {Hero} from './components/hero/hero';
+import {Cards} from './components/cards/cards';
+import {Footer} from './components/footer/footer';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, Toast],
-  templateUrl: './app.html',
+  imports: [
+    //RouterOutlet,
+    Header,
+    //Toast,
+    Hero,
+    Cards,
+    Footer
+  ],
+  template:`
+    <div>
+      <app-header/>
+      <main>
+        <app-hero/>
+        <app-cards/>
+      </main>
+      <app-footer/>
+    </div>`,
   styleUrl: './app.css'
 })
 export class App{
